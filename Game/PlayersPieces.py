2eeb114441ea632c1664615682a4e3ddd212efd8
= <shaul.chamoula@mail.huji.ac.il>
2020-12-09 13:52:31 +0200

    Primary implementation, based on the implementation of chess project (androidoss)

---
+++ Game/PlayersPieces.py
@@ -0,0 +1,20 @@
+from Game.Piece import Piece
+from Game.Tile import Tile
+
+
+class PlayersPieces:
+    def __init__(self, color):
+        self.my_pieces = self.init_pieces(color)
+
+    def init_pieces(self, color):
+        # TODO: Should I adopt the recommendation of PyCharm (PEP8?) to take init_pieces out of class player_pieces?
+        # TODO: assert color is either Color.White or Color.Black Enum. I may write it as base = 48 * get_color(color)
+        #  and make the assertion inside get_color(color)
+        base = 48 * color.value
+        return {base + k: Tile(base + k, Piece()) for k in [0, 2, 4, 6, 11, 13, 15]}
+
+    def __add__(self, tile):
+        self.my_pieces[tile.position] = tile
+
+    def __sub__(self, tile):
+        self.my_pieces.pop(tile.position)
---
+++ Game/Tile.py
@@ -0,0 +1,4 @@
+class Tile:
+    def __init__ (self, position, piece=None):
+        self.position = position
+        self.piece = piece
