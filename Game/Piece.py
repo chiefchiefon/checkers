94405fd683d8185c17cd29d7cdd49f70993c9a8f
= <shaul.chamoula@mail.huji.ac.il>
2020-12-09 15:16:26 +0200

    Enum class Degree added to Piece, and Piece attribute isKing changed to degree of Degree (initiated to Degree.PAWN).

--- Game/Piece.py
+++ Game/Piece.py
@@ -1,8 +1,16 @@
+from enum import Enum
+
+
+class Degree(Enum):
+    PAWN = 0
+    KING = 1
+
+
 class Piece:
     def __init__(self):
-        self.isKing = False
+        self.degree = Degree.PAWN
 
     def get_piece_degree(self):
-        if self.isKing:
+        if self.degree is Degree.KING:
             return "King"
         return "Pawn"
