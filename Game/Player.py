276e78b7358ee5ddc9fce140db566b1d3a266690
= <shaul.chamoula@mail.huji.ac.il>
2020-12-09 14:15:50 +0200

    Player class

---
+++ Game/Player.py
@@ -0,0 +1,12 @@
+from Game.PlayersPieces import PlayersPieces
+
+
+class Player:
+    def __init__(self, color):
+        self.name = self.set_name()
+        pieces = PlayersPieces(color)
+
+    def set_name(self, name=None):
+        if name is None:
+            return "Player" + self.__name__.toString()
+        return name
